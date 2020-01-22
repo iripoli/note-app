95e927f24a7d2a52fa1290b5f1837dcb30ef8c51
iripoli <iripoli96@gmail.com>
2020-01-22 15:53:11 +0100

    commit

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const app            = express();
 
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
+require('./routes/routes')(app, {})
+
 app.listen(port, ()=>{
     console.log('Listenting to port: ' + port)
 })
+
+
